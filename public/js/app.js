1f920ccdd036e791c64657506dbd4fe8f5218259
Dylan Smith <dylantsmith@gmail.com>
2013-05-12 19:59:43 -0700

    update the look of the game a bit

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,15 @@ function($, BoardView) {
 	"use-strict";
 
 	$(function() {
+		// render the game board
 		var boardView = new BoardView();
 		boardView.render();
+
+		// the timeout here is to delay adding the class until we've rendered
+		// to show an animation as the game board is loaded
+		setTimeout(function() {
+			// add our border lines animation
+			$("td").addClass("border-lines");
+		}, 100);
 	});
 });
